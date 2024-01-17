daeb89ed9688be8761b57fafbbffc3500e3db0c4
Yogi Firmansyah <yogifirmansyah618@gmail.com>
2024-01-17 21:08:06 +0700

    modified style.css, index.html and script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 function wide(){
+
     var s = document.getElementById('squareSide').value;
 
     var wide = s*s;
@@ -6,6 +7,7 @@ function wide(){
     document.getElementById('wide').innerHTML = wide;
 }
 function squarePerimeter(){
+
     var s = document.getElementById('squareSide').value;
 
     var squarePerimeter = 4*s;
@@ -13,6 +15,18 @@ function squarePerimeter(){
     document.getElementById('squarePerimeter').innerHTML = squarePerimeter;
 }
 function resetButton(){
-    document.getElementById('squareSide').value=''
+
+    document.getElementById('squareSide').value='';
+
+    document.getElementById('showResultLuas').style.display='none';
+
+    document.getElementById('showResultKel').style.display='none';
 }
- 
+function showResultLuas(){
+
+    document.getElementById('showResultLuas').style.display='block';
+}
+function showResultKel(){
+
+    document.getElementById('showResultKel').style.display='block';
+}
